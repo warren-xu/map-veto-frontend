63a20e9e02f70a870050c2bafa3440e6c10253c5
Warren <warrenxu2017@gmail.com>
2025-12-25 18:29:07 -0500

    feat: home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { CreateMatchPageComponent } from './create-match/create-match-page.compo
 import { JoinMatchPageComponent } from './join-match/join-match-page.component';
 import { MatchStatePageComponent } from './match-state/match-state-page.component';
 import { MatchPreviewComponent } from './match-summary/match-summary-page.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', component: CreateMatchPageComponent },
-  { path: 'create', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent},
+  { path: 'create', component: CreateMatchPageComponent },
   { path: 'join', component: JoinMatchPageComponent },
   { path: 'match/:id/team/:teamIndex', component: MatchStatePageComponent },
   { path: 'match/:id', component: MatchStatePageComponent },
